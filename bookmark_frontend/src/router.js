e672c23bea4e6ae13092e4a4d498f3da6bbaa9cc
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-02-17 12:37:16 -0500

    router.js done, just need variables from loaders

--- bookmark_frontend/src/router.js
+++ bookmark_frontend/src/router.js
@@ -1,1 +1,28 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
+import App from './App'
+import Index from './pages/Index'
+import Show from './pages/Show'
+import {} from './loaders'
+import { updateAction, createAction, deleteAction } from './actions'
+
+const router = createBrowserRouter(
+
+    createRoutesFromElements(
+
+        <Route path='/' element={<App/>}>
+
+            <Route path='' element={<Index/>}/>
+
+            <Route path=':id' element={<Show/>}/>
+
+            <Route path='create' element={createAction}/>
+
+            <Route path='update/:id' element={updateAction}/>
+
+            <Route path='delete/:id' element={deleteAction}/>
+
+        </Route>
+    )
+)
+
+export default router
