7cffd557a9328e83141ce89eb249a2d3d29b5d91
HZ <haleyzitzmann@gmail.com>
2024-02-17 15:08:07 -0700

    updated Index page for when there are no bookmarks

--- bookmark_frontend/src/pages/Index.js
+++ bookmark_frontend/src/pages/Index.js
@@ -2,6 +2,20 @@ import {Link, useLoaderData, Form} from "react-router-dom"
 
 const Index = () => {
     const bookmarks = useLoaderData();
+
+    if(!bookmarks){
+        return(
+            <div>
+                No bookmarks
+                <Form action='/create' method='post'>
+                    <input type='input' name='name' placeholder='website'/>
+                    <input type='input' name='url' placeholder='http://'/>
+                    <input type='input' name='description' placeholder='what does this website help with?'/>
+                    <input type='submit' value={'create bookmark'}/> 
+                </Form>
+            </div>
+        )
+    }
     
     return(
         <div>
