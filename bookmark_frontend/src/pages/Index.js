bc6b9e9078c01090829bd06a1a1a5c443d10cd61
HZ <haleyzitzmann@gmail.com>
2024-02-17 14:26:56 -0700

    fixed errors on show page

--- bookmark_frontend/src/pages/Index.js
+++ bookmark_frontend/src/pages/Index.js
@@ -1,4 +1,4 @@
-import {Link, useLoaaderData, Form} from "react-router-dom"
+import {Link, useLoaderData, Form} from "react-router-dom"
 
 const Index = () => {
     const bookmarks = useLoaderData();
