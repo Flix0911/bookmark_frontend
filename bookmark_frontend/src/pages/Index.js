d98ca545e45e55e7f47b1ff9d48d41c982595b38
Christian Alexander <christian.alexanderato@gmail.com>
2024-02-18 12:36:42 -0800

    begin styles

--- bookmark_frontend/src/pages/Index.js
+++ bookmark_frontend/src/pages/Index.js
@@ -5,26 +5,32 @@ const Index = () => {
 
   return (
     <div>
-      <h2>Bookmark List:</h2>
-      {/* made a correction to the variable inside the map by adding "()" around bookmark  */}
-      {bookmarks.map((bookmark) => {
-        return (
-          <div key={bookmark._id} className="bookmark">
-            <Link to={`/${bookmark._id}`}>
-              <h3>{bookmark.title}</h3>
-            </Link>
-          </div>
-        );
-      })}
+      
+      <div className="Bookmarks-list">
+        <h2>Bookmark List:</h2>
+        {/* made a correction to the variable inside the map by adding "()" around bookmark  */}
+        {bookmarks.map((bookmark) => {
+          return (
+            <div key={bookmark._id} className="bookmark">
+              <Link to={`/${bookmark._id}`}>
+                <h3>{bookmark.title}</h3>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
 
-      <h2>Add a new bookmark</h2>
-      <Form action="/create" method="post">
-        <input type="input" name="title" placeholder="website" />
-        <input type="input" name="url" placeholder="http://" />
-        {/* commented this input out of all forms that contain it as it is not a part of the Schema  */}
-        {/* <input type='input' name='description' placeholder='what does this website help with?'/> */}
-        <input type="submit" value={"Create Bookmark"} />
-      </Form>
+      <div className="create-form">
+        <h2>Add a new bookmark</h2>
+        <Form action="/create" method="post">
+          <input type="input" name="title" placeholder="website" />
+          <input type="input" name="url" placeholder="http://" />
+          {/* commented this input out of all forms that contain it as it is not a part of the Schema  */}
+          {/* <input type='input' name='description' placeholder='what does this website help with?'/> */}
+          <input type="submit" value={"Create Bookmark"} />
+        </Form>
+      </div>
+
     </div>
   );
 };
--- bookmark_frontend/src/pages/Show.js
+++ bookmark_frontend/src/pages/Show.js
@@ -4,24 +4,31 @@ const Show = () => {
   const bookmark = useLoaderData();
 
   return (
-    <div className="bookmark">
-      <h1>{bookmark.title}</h1>
-      <h2>{bookmark.url}</h2>
-      <p>{bookmark.description}</p>
+    <div>
       
-      {/* This form wont update  */}
-      <h3>Update {bookmark.title}</h3>
-      <Form action={`/update/${bookmark._id}`} method="post">
-        <input type="input" name="title" placeholder="website" />
-        <input type="input" name="url" placeholder="http://" />
-        {/* <input type='input' name='description' placeholder='what does this website help with?'/> */}
-        <input type="submit" value={`Update ${bookmark.title}`} />
-      </Form>
+      <div className="bookmark">
+        <h1>{bookmark.title}</h1>
+        <h2>{bookmark.url}</h2>
+        <p>{bookmark.description}</p>
+      </div>
 
-      <h3>Delete Bookmark</h3>
-      <Form action={`/delete/${bookmark._id}`} method="post">
-        <input type="submit" value={`Delete ${bookmark.title}`} />
-      </Form>
+      <div className="update-form">
+        <h3>Update {bookmark.title}</h3>
+        <Form action={`/update/${bookmark._id}`} method="post">
+          <input type="input" name="title" placeholder="website" />
+          <input type="input" name="url" placeholder="http://" />
+          {/* <input type='input' name='description' placeholder='what does this website help with?'/> */}
+          <input type="submit" value={`Update ${bookmark.title}`} />
+        </Form>
+      </div>
+
+      <div className="delete-button">
+        <h3>Delete Bookmark</h3>
+        <Form action={`/delete/${bookmark._id}`} method="post">
+          <input type="submit" value={`Delete ${bookmark.title}`} />
+        </Form>
+      </div>
+
     </div>
   );
 };
